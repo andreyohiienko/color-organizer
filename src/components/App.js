ed6d3dd47a2238016b77dd867b7a3ce01ef3e8de
Andrey Ohiienko <andreyohiienko@gmail.com>
2019-07-31 00:51:40 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import AppColorForm from './AppColorForm';
+import React from 'react';
+import '../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AppColorForm />
+    </div>
+  );
+}
+
+export default App;
