15537786d1ae4ca5bc91be91592028da21d87838
andrii-ohiienko <andrii.ohiienko@outright.digital>
2019-12-17 00:37:06 +0200

    removed state from starRating

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,43 +1,60 @@
 import React, { Component } from 'react'
 import Star from './Star'
 
-class StarRating extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      starsSelected: props.starsSelected || 0,
-    }
-    this.change = this.change.bind(this)
-  }
+const StarRating = ({ starsSelected = 0, totalStars = 5, onRate = f => f }) => (
+  <div className="star-rating">
+    {[...Array(totalStars)].map((n, i) => (
+      <Star
+        key={i}
+        selected={i < starsSelected}
+        onClick={() => onRate(i + 1)}
+      />
+    ))}
+    <p>
+      {starsSelected} of {totalStars} stars
+    </p>
+  </div>
+)
 
-  change(starsSelected) {
-    this.setState({ starsSelected })
-  }
+export default StarRating
 
-  render() {
-    const { totalStars } = this.props
-    const { starsSelected } = this.state
-    return (
-      <div className="star-rating">
-        {[...Array(totalStars)].map((n, i) => (
-          <Star
-            key={i}
-            selected={i < starsSelected}
-            onClick={() => this.change(i + 1)}
-          />
-        ))}
-        <p>
-          {starsSelected} of {totalStars} stars
-        </p>
-      </div>
-    )
-  }
-}
+// class StarRating extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       starsSelected: props.starsSelected || 0,
+//     }
+//     this.change = this.change.bind(this)
+//   }
 
-StarRating.PropTypes = {
-  totalStars: PropTypes.number,
-}
+//   change(starsSelected) {
+//     this.setState({ starsSelected })
+//   }
 
-StarRating.defaultProps = {
-  totalStars: 5,
-}
+//   render() {
+//     const { totalStars } = this.props
+//     const { starsSelected } = this.state
+//     return (
+//       <div className="star-rating">
+//         {[...Array(totalStars)].map((n, i) => (
+//           <Star
+//             key={i}
+//             selected={i < starsSelected}
+//           onClick={() => this.change(i + 1)}
+//           />
+//         ))}
+//         <p>
+//           {starsSelected} of {totalStars} stars
+//         </p>
+//       </div>
+//     )
+//   }
+// }
+
+// StarRating.PropTypes = {
+//   totalStars: PropTypes.number,
+// }
+
+// StarRating.defaultProps = {
+//   totalStars: 5,
+// }
