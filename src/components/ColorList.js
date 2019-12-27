24672ac2ffeb329a42a6ec73b82f487198e1db2f
andrii-ohiienko <andrii.ohiienko@outright.digital>
2019-12-28 01:27:58 +0200

    lens test 5

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -3,6 +3,4 @@ import React from 'react'
 const ColorList = ({ colors = [] }) => (
   <div className="color-list">{colors.length === 0 ? '' : ''}</div>
 )
-// merge test 2
-// test 4
-// test 3
+// git lens test 5
