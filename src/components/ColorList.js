481dcaf4b1006a53e5540eedfeda489da34953fb
andrii-ohiienko <andrii.ohiienko@outright.digital>
2019-12-28 01:33:07 +0200

    test 7

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -3,4 +3,5 @@ import React from 'react'
 const ColorList = ({ colors = [] }) => (
   <div className="color-list">{colors.length === 0 ? '' : ''}</div>
 )
-// git lens test 5
+// git
+// lens test 7
