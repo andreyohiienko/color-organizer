2159424b9a944f6582ed6cff1fd930e731f0fc4b
andrii-ohiienko <andrii.ohiienko@outright.digital>
2019-12-30 01:33:08 +0200

    add ability to check if component was update

--- src/components/Color.js
+++ src/components/Color.js
@@ -1,16 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
 import StarRating from './StarRating'
+import PropTypes from 'prop-types'
 import '../stylesheets/Color.scss'
 
-const Color = ({ title, color, rating = 0, onRemove=f=>f, onRate=f=>f }) => (
-  <section className="color">
-    <h1>{title}</h1>
-    <button onClick={onRemove}>X</button>
-    <div className="color" style={{ backgroundColor: color }}></div>
-    <div>
-      <StarRating starsSelected={rating} onRate={onRate} />
-    </div>
-  </section>
-)
+class Color extends Component {
+  componentWillMount() {
+    this.style = { backgroundColor: '#ccc' }
+  }
 
+  componentWillUpdate(nextProps) {
+    const { title, rating } = this.props
+    this.style = null
+    this.refs.title.style.backgroundColor = 'red'
+    this.refs.title.style.color = 'white'
+    alert(`${title}: rating ${rating} -> ${nextProps.rating}`)
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return this.props.rating !== nextProps.rating
+  }
+
+  componentDidUpdate(prevProps) {
+    const { title, rating } = this.props
+    const status = rating > prevProps.rating ? 'better' : 'worse'
+    this.refs.title.style.backgroundColor = ''
+    this.refs.title.style.color = 'black'
+  }
+
+  render() {
+    const { title, rating, color, onRemove, onRate } = this.props
+    return (
+      <section className="color" style={this.style}>
+        <h1 ref="title">{title}</h1>
+        <button onClick={onRemove}>X</button>
+        <div className="color" style={{ backgroundColor: color }}></div>
+        <div>
+          <StarRating starsSelected={rating} onRate={onRate} />
+        </div>
+      </section>
+    )
+  }
+}
+
+Color.propTypes = {
+  title: PropTypes.string,
+  rating: PropTypes.number,
+  color: PropTypes.string,
+  onRate: PropTypes.func,
+}
+
+Color.defaultProps = {
+  title: undefined,
+  rating: 0,
+  color: '#000',
+  onRate: f => f,
+  onRemove: f => f,
+}
 export default Color
