8f1b579f61dad389970ec29a00da6a6c93ab37a5
andrii-ohiienko <andrii.ohiienko@outright.digital>
2020-01-02 00:08:28 +0200

    add redux for app

---
+++ src/actions.js
@@ -0,0 +1,23 @@
+import C from './constants'
+import { v4 } from 'uuid'
+
+// action creators
+
+export const addColor = (title, color) => ({
+  type: C.ADD_COLOR,
+  id: v4(),
+  title,
+  color,
+  timestamp: new Date().toString(),
+})
+
+export const removeColor = id => ({
+  type: C.REMOVE_COLOR,
+  id,
+})
+
+export const rateColor = (id, rating) => ({
+  type: C.RATE_COLOR,
+  id,
+  rating,
+})
