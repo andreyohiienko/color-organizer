6fb9887a4c8ad5b48ae66477db759a1bfd229d44
andrii-ohiienko <andrii.ohiienko@outright.digital>
2020-02-04 00:53:51 +0200

    add new tests

---
+++ __tests__/components/containers/Menu.test.js
@@ -0,0 +1,37 @@
+import { Provider } from 'react-redux'
+import { compose } from 'redux'
+import { Menu } from '../../../src/components/containers'
+import { mount } from 'enzyme'
+
+jest.mock('../../../src/components/ui/SortMenu')
+
+describe('<Menu /> Container', () => {
+  let wrapper
+  const _store = {
+    dispatch: jest.fn(),
+    subscribe: jest.fn(),
+    getState: jest.fn(() => ({
+      sort: 'SORTED_BY_RATING',
+    })),
+  }
+
+  beforeAll(
+    () =>
+      (wrapper = mount(
+        <Provider store={_store}>
+          <Menu />
+        </Provider>,
+      )),
+  )
+
+  it('maps sort from state to props', () => {
+    wrapper
+      .find('SortMenuMock')
+      .props()
+      .onSelect('SORTED_BY_TITLE')
+    expect(_store.dispatch.mock.calls[0][0]).toEqual({
+      sortBy: 'SORTED_BY_TITLE',
+      type: 'SORT_COLORS',
+    })
+  })
+})
